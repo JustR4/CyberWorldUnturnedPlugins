cb44ffda4c301718a8e4571d4e1e9150ecec587e
Just R4 <56354448+JustR4@users.noreply.github.com>
2021-04-30 10:57:52 +0300

    CyberWorldUnturnedPlugin

---
+++ CyberWorldPlugins/Config.cs
@@ -0,0 +1,22 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberWorldPlugins
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public bool CyberWorldAdminTools;
+
+
+
+        public void LoadDefaults()
+        {
+            CyberWorldAdminTools = false;
+
+        }
+    }
+}
---
+++ CyberWorldPlugins/main.cs
@@ -0,0 +1,25 @@
+﻿using Rocket.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberWorldPlugins
+{
+    public class main : RocketPlugin<Config>
+    {
+        public static main instante;
+
+
+        protected override void Load()
+        {
+            base.Load();
+        }
+        protected override void Unload()
+        {
+            base.Unload();
+        }
+
+    }
+}
